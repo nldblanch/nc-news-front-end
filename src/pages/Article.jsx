7426d4f8b78baf97faf45b8c2ef1b41bc425d553
Nathan <nldblanch@gmail.com>
2024-08-05 16:13:40 +0100

    changed styling on header

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticleById } from "../api";
 import { Loading } from "../components/Loading";
-import "../css/Article.css"
+import "../css/Article.css";
 
 export const Article = () => {
   const { article_id } = useParams();
@@ -24,9 +24,8 @@ export const Article = () => {
         <h2>{article.title}</h2>
         <img src={article.article_img_url}></img>
         <div>
-
-        <h3>{article.author}</h3>
-        <p>on {new Date(`${article.created_at}`).toDateString()}</p>
+          <h3>{article.author}</h3>
+          <p>on {new Date(`${article.created_at}`).toDateString()}</p>
         </div>
         <p>{article.votes} Votes</p>
         <p>{article.body}</p>
