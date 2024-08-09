33de0a286354369e2adda833df39f7919c238fd1
Nathan <nldblanch@gmail.com>
2024-08-09 15:59:18 +0100

    added loading icon to comment names

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -5,20 +5,24 @@ import netlifyIdentity from "netlify-identity-widget";
 import { deleteComment, getUserByUsername } from "../api";
 import { FakeCommentCard } from "./FakeCommentCard";
 import { ErrorComponent } from "./ErrorComponent";
-import {VotesBarComments} from "./VotesBarComments"
+import { VotesBarComments } from "./VotesBarComments"
+import { Loading } from "./Loading"
 
 export const CommentCard = ({ comment }) => {
   const [hoverOnDelete, setHoverOnDelete] = useState(TrashCanRegular);
   const [isCommentDeleted, setIsCommentDeleted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingAuthor, setIsLoadingAuthor] = useState(true)
   const [error, setError] = useState(null);
   const [commentAuthor, setCommentAuthor] = useState()
   const user = netlifyIdentity.currentUser();
   const loggedInUser = user === null ? "" : user.email;
   useEffect(() => {
+    setIsLoadingAuthor(true)
     getUserByUsername(comment.author)
     .then((user) => {
       setCommentAuthor(user.name)
+      setIsLoadingAuthor(false)
     })
   }, [])
   const hover = () => {
@@ -51,7 +55,8 @@ export const CommentCard = ({ comment }) => {
     return (
       <div className="outline outline-1 outline-slate-200 mx-2 my-4 p-2 shadow-lg relative z-0">
         <div className="flex justify-between">
-          <h5 className="text-lg font-medium">{commentAuthor}</h5>
+          {isLoadingAuthor && <Loading />}
+          {!isLoadingAuthor && <h5 className="text-lg font-medium">{commentAuthor}</h5>}
           <p>on {new Date(`${comment.created_at}`).toDateString()}</p>
         </div>
         <p className="text-left">{comment.body}</p>
